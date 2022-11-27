36bb8843fc2682fb2d22a6d6f6001365935a7905
NataliyaNikulshina <victory515@yandex.ru>
2022-11-27 21:04:03 +0300

    fix: fix perfect pixel

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -8,26 +8,26 @@ class AppHeader extends React.Component {
         <header className={`${appHeader.header}`}>
           <nav className={`${appHeader.container} pt-4 pb-4`}>
             <ul className={appHeader.nav}>
-                    <ul className={appHeader.items}>
-                      <li className={`${appHeader.item} pl-5 pr-4 mr-2`}>
-                          <BurgerIcon type="primary" />
-                          <p className='ml-2'>Конструктор</p>
-                      </li>
-                      <li className={`${appHeader.item} pl-5 pr-5 `}>
-                          <ListIcon type="secondary" />
-                          <p className={`${appHeader.text} ml-2`}>Лента заказов</p>
-                      </li>
-                    </ul>
-                    <li className={appHeader.item}>
-                      <div className={appHeader.logo}>
-                        <Logo /> 
-                      </div>
-                    </li>
-                    <li className={`${appHeader.item} pl-5 pr-5 `}>
-                        <ProfileIcon type="secondary" />
-                        <p className={`${appHeader.text} ml-2`}>Личный кабинет</p>
-                    </li>
-                </ul>
+               <ul className={appHeader.items}>
+                  <a className={`${appHeader.item} pl-5 pr-4 mr-2`}>
+                    <BurgerIcon type="primary" />
+                    <p className='ml-2'>Конструктор</p>
+                  </a>
+                   <a className={`${appHeader.item} pl-5 pr-5 `}>
+                    <ListIcon type="secondary" />
+                    <p className={`${appHeader.text} ml-2`}>Лента заказов</p>
+                  </a>
+               </ul>
+                  <li className={appHeader.item}>
+                    <div className={appHeader.logo}>
+                      <Logo /> 
+                    </div>
+                  </li>
+                    <a className={`${appHeader.item} pl-5 pr-5 `}>
+                      <ProfileIcon type="secondary" />
+                      <p className={`${appHeader.text} ml-2`}>Личный кабинет</p>
+                    </a>
+            </ul>
           </nav> 
         </header>
  );
--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -5,8 +5,7 @@ import {dataIngredients} from '../../utils/data.js';
 
 function BurgerFirstItem ({ingredient}){
   return (
-    <li className={`${burgerConstructor.item} mr-4 mb-4 ml-4`}>
-      <li className={`${burgerConstructor.element} ml-8`}>
+      <div className={`${burgerConstructor.element} mr-4 mb-4`}>
       <ConstructorElement
         type='top'
         isLocked={true}
@@ -14,31 +13,29 @@ function BurgerFirstItem ({ingredient}){
         price={ingredient.price}
         thumbnail={ingredient.image}
         />
-       </li>
-    </li>
+       </div>
   )
 }
 
 function BurgerMiddleItem ({ingredient}){
   return (
-    <li className={`${burgerConstructor.item} mr-4 mb-4 ml-4`}>
+    <li className={`${burgerConstructor.item} mr-2 mb-4 ml-4`}>
       <DragIcon type="primary"/>
-      <li className={`${burgerConstructor.element}`}>
+      <div className={`${burgerConstructor.element}`}>
         <ConstructorElement 
           type=''
           text={`${ingredient.name}`} 
           price={ingredient.price}
           thumbnail={ingredient.image}
           />
-      </li>
+      </div>
     </li>
   )
 }
 
 function BurgerLastItem ({ingredient}){
   return (
-    <li className={`${burgerConstructor.item} mr-4 mb-4 ml-4`}>
-      <li className={`${burgerConstructor.element} ml-8`}>
+    <div className={`${burgerConstructor.element} mr-4 mt-4`}>
         <ConstructorElement
         type='bottom'
         isLocked={true}
@@ -46,28 +43,37 @@ function BurgerLastItem ({ingredient}){
         price={ingredient.price}
         thumbnail={ingredient.image}
         />
-      </li>
-    </li>
+      </div>
   )
 }
 
 class BurgerConstructor extends React.Component {
-    render(
-     
-    ) {
+    render() {
+      const bun = dataIngredients.find(function (el) { 
+        return el.type === "bun";
+      });
       return (
-        <section className={`${burgerConstructor.container} mt-15 mb-1`}>
-        <ul className={`${burgerConstructor.list}`}>
-          {dataIngredients.map((el) => { 
-            if (el.type === "bun") {
-              return (<BurgerFirstItem ingredient={el} />);
-            } else 
-            return (<BurgerMiddleItem ingredient={el} />);
-          })}
-        </ul>
-      <Button htmlType="button" type="primary" size="large">
-          Оформить заказ
-        </Button> 
+        <section className={`${burgerConstructor.container} mt-15`}>
+          <BurgerFirstItem ingredient={bun} key={bun.id}/>
+          <ul className={`${burgerConstructor.list}`}>
+            {dataIngredients.map((el) => { 
+              if (el.type !== "bun") {
+                return (<BurgerMiddleItem ingredient={el} key={el.id}/>);
+              } 
+            })}
+          </ul>
+          <BurgerLastItem ingredient={bun} key={bun.id}/>
+          <ul className={`${burgerConstructor.result} mt-10`}>
+            <p className="text text_type_digits-medium">610</p>
+            <li className={`${burgerConstructor.icon} ml-2 mr-10`}>
+              <CurrencyIcon type="primary"/>
+            </li>
+            <li className="mr-4">
+              <Button htmlType="button" type="primary" size="large">
+                Оформить заказ
+              </Button> 
+            </li>
+          </ul>
         </section>   
       );
 }
