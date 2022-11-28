3712fd03698b02ad4e0b6b5990e8be0fa6414947
NataliyaNikulshina <victory515@yandex.ru>
2022-11-28 17:46:35 +0300

    fix: fixed isRequired

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 export const ingredientType = PropTypes.shape({
-  name: PropTypes.string,
-  image: PropTypes.string,
-  price: PropTypes.number,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 });
