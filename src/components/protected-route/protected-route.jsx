d7ec55ff8f85dbc41ede0eed053e089a04027d3d
NataliyaNikulshina <victory515@yandex.ru>
2023-02-17 22:41:53 +0300

    feat: added inrediend-id page and propTypes

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,5 +1,11 @@
 import { Navigate } from "react-router-dom";
+import propTypes from "prop-types";
 
 export default function ProtectedRouteElement({ children, isAuth }) {
   return ( isAuth ? children : <Navigate to={'/'} />);
+}
+
+ProtectedRouteElement.propTypes = {
+  children: propTypes.element.isRequired,
+  isAuth: propTypes.bool.isRequired,
 }
--- src/pages/ingredients-id.jsx
+++ src/pages/ingredients-id.jsx
@@ -1,31 +1,55 @@
 import ingredientDetails from "../components/ingredient-details/ingredient-details.module.css";
 import IngredientDetails from "../components/ingredient-details/ingredient-details";
+import HomePage from "../pages/home";
 import AppHeader from "../components/app-header/app-header";
-import {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import getItems from '../services/actions/ingredients';
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useLocation } from "react-router-dom";
+import { getProductData } from "../utils/api";
+import { SET_INGREDIENT_DETAILS } from '../services/actions/ingredient-details';
 
 const IngredientDetailsPage = () => {
- // const [ingredient, setIngredient] = useState(null);
- const ingredients = useSelector((state) => state.ingredients);
+  //const [ingredient, setIngredient] = useState(null);
+  const dispatch = useDispatch()
+  const location = useLocation();
+  const ingredients = useSelector((state) => state.details.ingredientDetails);
   const { id } = useParams();
-  const ingDetails = {};
-  
-  useEffect(() => {
-     ingDetails = ingredients.items.filter(ing => ing._id === id)  
-},[id])
+  console.log(ingredients)
 
+  useEffect(() => {
+    if (location.state?.from !== "/") {
+      getProductData()
+        .then((ingredients) => {
+          console.log(ingredients);
+          console.log(id);
+          const ingredient = ingredients.data.filter(
+            (ingredient) => ingredient._id === id
+          )[0];
+          console.log(ingredient);
+          dispatch({type: SET_INGREDIENT_DETAILS, payload: ingredient})
+          //setIngredient(ingredient);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [id, location.state]);
 
   return (
     <>
       <AppHeader />
-      <div className={ingredientDetails.container}>
-        <IngredientDetails
-          ingredientDetails={ingDetails}
-          textAlignCentre={true}
-        />
-      </div>
+      {location.state?.from === "/" ? (
+        <HomePage />
+      ) : (
+        <div className={`${ingredientDetails.container} mt-30`}>
+        <div className={ingredientDetails.main}>
+        <h1
+        className={`text text_type_main-large pt-2`}>Детали
+        ингредиента</h1>
+          <IngredientDetails
+            textAlignCentre={true}
+          />
+        </div>
+        </div>
+      )}
     </>
   );
 };
