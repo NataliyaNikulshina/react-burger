e1650f3f3e2d45f38cfd2077badcde07c24ac089
NataliyaNikulshina <victory515@yandex.ru>
2023-03-17 21:55:10 +0300

    feat: added ts for action constructor

---
+++ src/services/actions/constructor.ts
@@ -0,0 +1,37 @@
+import { IIngredient } from "../types/data";
+
+export const ADD_ING_CONSTRUCTOR: 'ADD_ING_CONSTRUCTOR' = "ADD_ING_CONSTRUCTOR";
+export const SORT_ING_CONSTRUCTOR: 'SORT_ING_CONSTRUCTOR' = "SORT_ING_CONSTRUCTOR";
+export const DELETE_ING_CONSTRUCTOR: 'DELETE_ING_CONSTRUCTOR' = "DELETE_ING_CONSTRUCTOR";
+export const SET_BIN_CONSTRUCTOR: 'SET_BIN_CONSTRUCTOR' = "SET_BIN_CONSTRUCTOR";
+export const RESET_ING_CONSTRUCTOR: 'RESET_ING_CONSTRUCTOR' = "RESET_ING_CONSTRUCTOR";
+
+export interface IAddIngredientConstructorAction {
+  readonly type: typeof ADD_ING_CONSTRUCTOR;
+  readonly payload: IIngredient;
+}
+export interface ISortIngredientConstructorAction {
+  readonly type: typeof SORT_ING_CONSTRUCTOR;
+  payload: {prevIndex: number, nextIndex: number}
+}
+export interface IDeleteIngredientConstructorAction {
+  readonly type: typeof DELETE_ING_CONSTRUCTOR;
+  readonly payload: string | undefined;
+}
+export interface IResetIngredientConstructorAction {
+  readonly type: typeof RESET_ING_CONSTRUCTOR;
+}
+export interface ISetBinConstructorAction {
+  readonly type: typeof SET_BIN_CONSTRUCTOR;
+  readonly payload: IIngredient;
+}
+
+export const addIngConstructor = (ingredient: IIngredient): IAddIngredientConstructorAction => ({type: ADD_ING_CONSTRUCTOR, payload: ingredient})
+export const deleteIngConstructor = (ingId: string | undefined): IDeleteIngredientConstructorAction => ({type: DELETE_ING_CONSTRUCTOR, payload: ingId})
+export const setBunConstructor = (bun: IIngredient): ISetBinConstructorAction => ({type: SET_BIN_CONSTRUCTOR, payload: bun})
+export const sortIngConstructor = (dragIndex: number, hoverIndex: number) => ({type: SORT_ING_CONSTRUCTOR, payload: {
+    from: dragIndex,
+    to: hoverIndex,
+  }})
+export const resetIngConstructor = () => ({type: RESET_ING_CONSTRUCTOR})
+
